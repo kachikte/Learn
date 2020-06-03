2a9a5bc517384fc0bb5dc107212b158846bf31d0
Kachy O <kachitheengineer@gmail.com>
2020-06-03 09:39:08 +0100

    Restring pages to the users that own them

--- Learn/views.py
+++ Learn/views.py
@@ -4,27 +4,33 @@ from .models import Topic, Entry
 
 from .forms import TopicForm, EntryForm
 
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 
 from django.urls import reverse
+
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def index(request):
 	return render(request, 'Learn/index.html')
 
+@login_required
 def topics(request):
-	topics = Topic.objects.order_by('date_added')
+	topics = Topic.objects.filter(owner = request.user).order_by('date_added')
 	context = {'topics': topics}
 	return render(request, 'Learn/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
 	topic = Topic.objects.get(id=topic_id)
 	entry = topic.entry_set.order_by('-date_added')
+	if topic.owner != request.user:
+		raise Http404
 	context = {'topic': topic, 'entry': entry}
 	return render(request, 'Learn/topic.html', context)
 
-
+@login_required
 def add_topic(request):
 	if (request.method!='POST'):
 		form = TopicForm()
@@ -32,13 +38,15 @@ def add_topic(request):
 	else:
 		form = TopicForm(request.POST)
 		if form.is_valid():
-			form.save()
+			new_topic = form.save(commit=False)
+			new_topic.owner = request.user
+			new_topic.save()
 			return HttpResponseRedirect(reverse('Learn:topics'))
 
 	context = {'form': form}
 	return render(request, 'Learn/add_topic.html', context)
 
-
+@login_required
 def add_entry(request, topic_id):
 	topic = Topic.objects.get(id=topic_id)
 	if (request.method!='POST'):
@@ -54,10 +62,12 @@ def add_entry(request, topic_id):
 	context = {'form': form, 'topic': topic}
 	return render(request, 'Learn/add_entry.html', context)
 
-
+@login_required
 def edit_entry(request, entry_id):
 	entry = Entry.objects.get(id=entry_id)
 	topic = entry.topic
+	if topic.owner != request.user:
+		raise Http404
 	if (request.method!='POST'):
 		form = EntryForm(instance=entry)
 
